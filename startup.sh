8eb1aa3a9f06178e5d594dde0d8f7482f3629012
mani14713 <mabdulrehman14713@gmail.com>
2024-07-13 20:21:40 +0500

    added permission

--- startup.sh
+++ startup.sh
@@ -6,11 +6,11 @@ NGINX_CONF="/etc/nginx/sites-available/default"
 # Update Nginx configuration to set the document root to the public directory of Laravel
 cat <<EOL > $NGINX_CONF
 server {
-    listen 8080;
-    listen [::]:8080;
-    root /var/www/html/wwwroot;
+    listen 80;
+    listen [::]:80;
+    root /var/www/html/wwwroot/public;
     index index.php index.html index.htm;
-    server_name example.com www.example.com;
+    server_name atthehouseorgapi.azurewebsites.net;
     port_in_redirect off;
 
     location / {
@@ -28,25 +28,25 @@ server {
         log_not_found off;
     }
 
-    location ~* [^/]\.php(/|$) {
-        fastcgi_split_path_info ^(.+?\.[Pp][Hh][Pp])(|/.*)$;
-        fastcgi_pass 127.0.0.1:9000;
-        include fastcgi_params;
-        fastcgi_param HTTP_PROXY "";
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php8.2-fpm.sock;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        fastcgi_param PATH_INFO \$fastcgi_path_info;
-        fastcgi_param QUERY_STRING \$query_string;
-        fastcgi_intercept_errors on;
-        fastcgi_connect_timeout 300;
-        fastcgi_send_timeout 3600;
-        fastcgi_read_timeout 3600;
-        fastcgi_buffer_size 128k;
-        fastcgi_buffers 4 256k;
-        fastcgi_busy_buffers_size 256k;
-        fastcgi_temp_file_write_size 256k;
+        include fastcgi_params;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
     }
 }
 EOL
+
+# Set correct permissions
+find /var/www/html/wwwroot -type d -exec chmod 755 {} \;
+find /var/www/html/wwwroot -type f -exec chmod 644 {} \;
+
+# Ensure correct ownership
+chown -R www-data:www-data /var/www/html/wwwroot
 
 # Restart Nginx to apply the changes
 service nginx restart
