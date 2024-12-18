f6c3cd75efa1f2d2a0300771770e3d9f70ace177
sgsringo <sgsringo@gmail.com>
2024-12-18 21:13:37 +0900

    updated

---
+++ startup.sh
@@ -0,0 +1,10 @@
+cp /home/site/wwwroot/default /etc/nginx/sites-available/default
+chown -R www-data:www-data /var/www/html/wwwroot
+cd /home/site/wwwroot/ 
+
+php artisan cache:clear
+php artisan route:clear
+php artisan config:cache
+php artisan config:clear
+
+service nginx reload
